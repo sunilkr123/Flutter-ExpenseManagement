334914e92a6319be9f46913f5d9fdb852be8850c
sunilkumarsah6 <sunilkumarsah6@gmail.xom>
2022-12-08 19:04:08 +0530

    added list view

--- lib/transaction_list.dart
+++ lib/transaction_list.dart
@@ -3,44 +3,48 @@ import 'package:intl/intl.dart';
 import './transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  final List<TransactionData> userTransaction;
 
+  final List<TransactionData> userTransaction;
   TransactionList(this.userTransaction);
 
   @override
   Widget build(BuildContext context) {
-    return Column(children:
-    userTransaction.map((txt) {
-      return Card(
-        child: Row(children: [
-          Container(
-            child:
-            Text(
-              '\$${txt.amount.toString()}',
-              style: TextStyle(fontSize: 20,
-                  color: Colors.pink,
-                  fontWeight: FontWeight.bold),
-            ),
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.pink, width: 2,),
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(txt.title,
-                style: TextStyle(color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16),),
+
+    return Container(
+     height: 500,
+      child: ListView.builder(itemBuilder: (ctx, index) {
+        return Card(
+          child: Row(children: [
+            Container(
+              child:
               Text(
-                DateFormat('yyyy-MMM-dd, MM:hh a').format(txt.date),
-                style: TextStyle(color: Colors.grey,),)
-            ],)
-        ],),
-      );
-    }).toList(),
+                '\$${userTransaction[index].amount.toString()}',
+                style: TextStyle(fontSize: 20,
+                    color: Colors.pink,
+                    fontWeight: FontWeight.bold),
+              ),
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.pink, width: 2,),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(userTransaction[index].title,
+                  style: TextStyle(color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),),
+                Text(
+                  DateFormat('yyyy-MMM-dd, MM:hh a').format(
+                      userTransaction[index].date),
+                  style: TextStyle(color: Colors.grey,),)
+              ],)
+          ],),
+        );
+      }, itemCount: userTransaction.length,
+      ),
     );
   }
 }
